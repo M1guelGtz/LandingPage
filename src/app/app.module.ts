72bae2898361ef293ac589fa6b9847c04d276ac2
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-09-22 23:19:14 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Home/nav/nav.component';
+import { BodyComponent } from './Home/body/body.component';
+import { AlbumsComponent } from './Home/albums/albums.component';
+import { FooterComponent } from './Home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BodyComponent,
+    AlbumsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

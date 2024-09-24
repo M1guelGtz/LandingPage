e3d17bbf4f0438a7da61d85425d06e5c852cc83f
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-09-24 12:35:36 -0600

    add: Componente de los albums y comentarios

--- src/app/Home/albums/albums.component.ts
+++ src/app/Home/albums/albums.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Album } from '../Interfaces/album';
 
 @Component({
   selector: 'app-albums',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './albums.component.css'
 })
 export class AlbumsComponent {
+  albums : Album[] = [
+    {id : 1, title : "Homegrizzy Boys", img : "HomeGrizzyBoys.jpeg"},
+    {id : 2, title : "Homegrizzylies", img : "HomeGrizzyLies.jpeg"},
+    {id : 3, title : "Fantasma", img : "Fantasmas.jpeg"},
+    {id : 4, title : "Por el $", img : "PorEl.jpeg"},
+    {id : 5, title : "No te confundas", img : "NoTeConfundas.jpeg"},
+    {id : 6, title : "Blanca naviidad", img : "BlancaNavidad.png"}
+  ]
+  comentarios: string[] = [
+    "Este album es muy bueno, sinceramente me gusta la letra de la cancion",
+    "Este album es muy malo",
+    "Este album es muy bueno",
+    "Este album es muy malo",
+  ] 
 
 }
